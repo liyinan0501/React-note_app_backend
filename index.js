cb5904b91c4af0cb22ededeb5b7dd2e79b9f04b2
Yinan Li <gabriel.li5566@gmail.com>
2022-05-23 22:18:36 +0300

    notebackend server

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(cors())
+app.use(bodyParser.json())
+
+let notes = [
+  {
+    id: 1,
+    content: 'HTML on helppoa',
+    date: '2017-12-10T17:30:31.098Z',
+    important: true,
+  },
+  {
+    id: 2,
+    content: 'Selain pystyy suorittamaan vain javascriptiä',
+    date: '2017-12-10T18:39:34.091Z',
+    important: false,
+  },
+  {
+    id: 3,
+    content: 'HTTP-protokollan tärkeimmät metodit ovat GET ja POST',
+    date: '2017-12-10T19:20:14.298Z',
+    important: true,
+  },
+  {
+    id: 4,
+    content: 'Testing',
+    date: '2017-12-10T19:20:14.298Z',
+    important: true,
+  },
+]
+
+const generateId = () => {
+  const maxId =
+    notes.length > 0
+      ? notes
+          .map((n) => n.id)
+          .sort((a, b) => a - b)
+          .reverse()[0]
+      : 1
+  return maxId + 1
+}
+
+app.post('/notes', (req, res) => {
+  const body = req.body
+
+  if (body.content === undefined) {
+    return res.status(400).json({ error: 'content missing' })
+  }
+
+  const note = {
+    id: generateId(),
+    content: body.content,
+    date: new Date(),
+    important: body.important || false,
+  }
+
+  notes = notes.concat(note)
+  res.json(note)
+})
+
+app.get('/', (req, res) => {
+  res.send(`<h1>Hello World!</h1>`)
+})
+
+app.get('/notes', (req, res) => {
+  res.json(notes)
+})
+
+app.get('/notes/:id', (req, res) => {
+  const id = +req.params.id
+  const note = notes.find((note) => note.id === id)
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/notes/:id', (req, res) => {
+  const id = +req.params.id
+  notes = notes.filter((note) => note.id !== id)
+  res.status(204).end()
+})
+
+const error = (req, res, next) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+  next()
+}
+app.use(error)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
